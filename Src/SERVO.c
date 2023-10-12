df5817879e8bf25f9450dc8ba94ba41bd7510671
Dominic Merchelski <dm5441@rit.edu>
2023-10-12 11:30:15 -0400

    deleted uneccessary fields

--- Src/SERVO.c
+++ Src/SERVO.c
@@ -27,9 +27,6 @@ SERVO_Cfg SERVO_Cfg_1= // Servo Motor 1 Configurations
 	0,
 	0,
 	0,
-	0,
-	0,
-	0,
 	status_paused,
 	state_waiting
 };
@@ -44,9 +41,6 @@ SERVO_Cfg SERVO_Cfg_2= // Servo Motor 2 Configurations
     1000,
     0.65,
     2.3,
-	0,
-	0,
-	0,
 	0,
 	0,
 	0,
